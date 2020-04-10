ed7c1ce1ae0475843624d4274ec181970b87ecc5
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-10 10:12:40 +0200

    Update Day 1 Data Types.js

--- Day 1 Data Types.js
+++ Day 1 Data Types.js
@@ -1,11 +1,19 @@
-// Declare second integer, double, and String variables.
-var i2, d2, s2;
-// Read and save an integer, double, and String to your variables.
-i2 = 12; d2 = 4.0; s2 = "is the best place to learn and practice coding!";
-// Print the sum of both integer variables on a new line.
-console.log(i+i2);
-// Print the sum of the double variables on a new line.
-console.log((d+d2).toFixed(1));
-// Concatenate and print the String variables on a new line
-// The 's' variable above should be printed first.
-console.log(s+s2);
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+// START FROM HERE //
+    // Declare second integer, double, and String variables.
+    var i2, d2, s2;
+    // Read and save an integer, double, and String to your variables.
+    i2 = parseInt(readLine());
+    d2 = parseFloat(readLine());
+    s2 = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(i+i2);
+    // Print the sum of the double variables on a new line.
+    console.log((d+d2).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+s2);
+}
