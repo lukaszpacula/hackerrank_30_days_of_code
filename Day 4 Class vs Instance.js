61f47bb0f62233e79c47cf2d234e397a8577136e
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-13 13:07:38 +0200

    Create Day 4 Class vs Instance.js

---
+++ Day 4 Class vs Instance.js
@@ -0,0 +1,21 @@
+function Person(initialAge){
+    var age = initialAge;
+    
+  this.amIOld=function(){
+
+    if (age < 0) {
+        console.log("Age is not valid, setting age to 0.");
+        age = 0;
+        this.amIOld();
+    } else if (age > -1 && age < 13) {
+        console.log("You are young.");
+    } else if (age >= 13 && age < 18) {
+        console.log("You are a teenager.");
+    } else {
+        console.log("You are old.");
+    }
+  };
+   this.yearPasses=function(){
+          age++;
+   };
+}
