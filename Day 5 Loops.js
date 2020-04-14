d21c219697eb41de348007208b6e7b437829555f
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-14 09:37:16 +0200

    Update Day 5 Loops.js

--- Day 5 Loops.js
+++ Day 5 Loops.js
@@ -2,7 +2,7 @@ function main() {
     const n = parseInt(readLine(), 10);
     var i, result;
     for (i = 1; i < 11; i++) {
-        result = n*i;
-        console.log(n.toString() + " x " + i + " = " + result.toString());
+        result = n * i;
+        console.log(n + " x " + i + " = " + result);
     }
 }
