d4ea33b1f48d0b7d829007217a0d0030ed9de7dc
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-24 09:48:06 +0200

    Create Day 15 Linked List.js

---
+++ Day 15 Linked List.js
@@ -0,0 +1,10 @@
+this.insert=function(head,data){
+        if (head === null) {
+            return new Node(data);
+        } else if(head.next === null) {
+            head.next = new Node(data);
+        } else {
+            this.insert(head.next, data);
+        }
+        return head;
+    };
