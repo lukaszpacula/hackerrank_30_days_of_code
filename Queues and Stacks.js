05cbdf1fbb106eccdc3cfc20bd94136681ae7ab6
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-27 19:32:53 +0200

    Create Queues and Stacks.js

---
+++ Queues and Stacks.js
@@ -0,0 +1,8 @@
+function Solution(){
+    this.stack = [];
+    this.queue = [];
+    Solution.prototype.pushCharacter = this.stack.push;
+    Solution.prototype.popCharacter = this.stack.pop;
+    Solution.prototype.enqueueCharacter = this.queue.push;
+    Solution.prototype.dequeueCharacter = this.queue.shift;
+}
