090a8c070853821612677a55a3094e0d102f6e47
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-28 12:08:52 +0200

    Delete Day 19 Interfaces.c

--- Day 19 Interfaces.c
+++
@@ -1,9 +0,0 @@
-public class Calculator: AdvancedArithmetic {
-    public int divisorSum(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) sum += i; to sum.
-        }
-        return sum;
-    }
-}
