003f9a78145e5165e8c6576de0b59010441ef8a3
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-30 09:58:45 +0200

    Update Day 21 Generics.java

--- Day 21 Generics.java
+++ Day 21 Generics.java
@@ -1,5 +1,5 @@
-public static <E> void printArray(E[] generic){
-    for(E element : generic) {
+public static <E> void printArray(E[] generic) {
+    for (E element : generic) {
         System.out.println(element); 
     }
 }
