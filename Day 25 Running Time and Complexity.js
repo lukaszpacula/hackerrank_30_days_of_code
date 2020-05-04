eba0a64cb11a53b767c6eb274a749a55c71b006d
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-05-04 10:06:58 +0200

    Create Day 25 Running Time and Complexity.js

---
+++ Day 25 Running Time and Complexity.js
@@ -0,0 +1,20 @@
+function processData(input) {
+var result = input.split("\n").slice(1).map(isPrime);
+    function isPrime(input, i) {
+        var txt = ["Not prime", "Prime"];
+        if (input == 1) {
+            return txt[0];
+        } else if (input == 2) {
+            return txt[1];
+        } else if (input % 2 == 0) {
+            return txt[0];
+        } 
+        for (i = 3; i <= Math.sqrt(input); i += 2) {
+            if (input % i == 0) {
+                return txt[0];
+            }
+        }
+        return txt[1];
+    }
+    console.log(result.join("\n"));
+}
